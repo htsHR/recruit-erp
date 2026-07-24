ef79c687b1453310a07e9bd2cd58cb0e97526934
htsHR <wjdrntks9@naver.com>
2026-07-24 12:41:03 +0900

    Add files via upload

--- js/state-init.js
+++ js/state-init.js
@@ -6,4 +6,4 @@ applicants = load();
 calendarEvents = loadCalendarEvents();
 hireWaitingProfiles = loadHireWaitingProfiles();
 messageTemplates = typeof loadMessageTemplates==='function' ? loadMessageTemplates() : [];
-console.info('[HOME_DEV] Recruit ERP v10.47.7 loaded applicants:', applicants.length, 'hire waiting profiles:', hireWaitingProfiles.length, 'message templates:', messageTemplates.length);
+console.info('[HOME_DEV] Recruit ERP v10.47.8 loaded applicants:', applicants.length, 'hire waiting profiles:', hireWaitingProfiles.length, 'message templates:', messageTemplates.length);
